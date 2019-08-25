5d4dd534a498b4f69e2a60b5d80f083a6d0c9b94
jubin21 <jubin@mymusictaste.com>
2019-08-25 20:07:37 +0900

    setting socket

--- web/istick-app/src/App.js
+++ web/istick-app/src/App.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
 import Joystick from 'react-joystick';
+import ready from './assets/deploy-ready.gif';
+import deploy from './assets/deploy-done.gif';
 
-const joyOptions = {
-  mode: 'static',
-  catchDistance: 150,
-  color: 'white'
-}
-
-const containerStyle = {
-  position: 'relative',
-  height: '700px',
-  width: '100%',
-  background: 'linear-gradient(to right, #E684AE, #79CBCA, #77A1D3)'
-}
 
 class App extends Component {
-  constructor() {
-    super();
-    this.managerListener = this.managerListener.bind(this);
+  componentDidMount() {
+    this.setupSocket();
   }
 
-  managerListener(manager) {
-    manager.on('move', (e, stick) => {
-        console.log('I moved!')
-    })
-    manager.on('end', () => {
-        console.log('I ended!')
-    })
+  setupSocket(channel) {
+    const url = "ws://localhost:8000";
+    this.socket = new WebSocket(url);
+    this.socket.onopen = (event) => {
+        console.log(`Socket is connected to "${url}"`)
+    };
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      console.log(data);
+    }
+  }
+
+  sendToSocket(message) {
+    this.socket.send(JSON.stringify(message));
+  }
+
+  closeSocket() {
+    this.socket.close();
+  }
+
+  componentWillUnmount() {
+    this.closeSocket();
   }
 
   render() {
-    const { classes } = this.props;
     return (
+      <div>
+      <image src={ready} classNmae='effect_section'/>
         <div>
-            <Joystick joyOptions={joyOptions} containerStyle={containerStyle} managerListener={this.managerListener} />
+         input secgtion
         </div>
+      </div>
     )
   }
 }
